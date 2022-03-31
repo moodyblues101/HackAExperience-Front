415d0d9a0de5e96f46c4ccc0e85a337f1d6762f0
moodyblues101 <joe.gmx@hotmail.com>
2022-03-31 18:31:52 +0200

    Homepage added

---
+++ hackaexperience/src/pages/ExperiencesPage/index.js
@@ -0,0 +1,11 @@
+import './style.css';
+
+const ExperiencesPage = () => {
+    return (
+        <div>
+            <h1>ExperiencesPage</h1>
+        </div>
+    );
+}
+
+export default ExperiencesPage;
